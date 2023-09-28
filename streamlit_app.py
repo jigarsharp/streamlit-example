299d96724681525ecad54353b8458dab5ba283ba
jigarsharp <146315274+jigarsharp@users.noreply.github.com>
2023-09-28 23:01:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,60 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import numpy as np
 import streamlit as st
+import tensorflow as tf
+from PIL import Image, ImageOps
 
-"""
-# Welcome to Streamlit!
+def import_and_predict(image_data, model):
+    
+        size = (75,75)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = image.convert('RGB')
+        image = np.asarray(image)
+        image = (image.astype(np.float32) / 255.0)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+        img_reshape = image[np.newaxis,...]
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+        prediction = model.predict(img_reshape)
+        
+        return prediction
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+model = tf.keras.models.load_model('best_model.hdf5')
 
+st.write("""
+         # Food item Prediction
+         """
+         )
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.write("This is a simple image classification web app to predict food item name")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
+#
+if file is None:
+    st.text("You haven't uploaded an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    
+    if np.argmax(prediction) == 0:
+        st.write("It is a apple pie!")
+    elif np.argmax(prediction) == 1:
+        st.write("It is a bibimbap!")
+    elif np.argmax(prediction) == 2:
+        st.write("It is a cannoli!")
+    elif np.argmax(prediction) == 3:
+        st.write("It is a edamame!")
+    elif np.argmax(prediction) == 4:
+        st.write("It is a falafel!")
+    elif np.argmax(prediction) == 5:
+        st.write("It is a french toast!")
+    elif np.argmax(prediction) == 6:
+        st.write("It is a ice cream!")
+    elif np.argmax(prediction) == 7:
+        st.write("It is a ramen!")
+    elif np.argmax(prediction) == 8:
+        st.write("It is a sushi!")
+    else:
+        st.write("It is a tiramisu!")
+    
+    st.text("Probability (0: apple pie, 1: bibimbap, 2: cannoli, 3: edamame, 4: falafel, 5: toast, 6: ice cream, 7: ramen, 8: sushi, 9: tiramisu")
+    st.write(prediction)
